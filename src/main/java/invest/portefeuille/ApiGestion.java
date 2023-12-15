7c8db661c6d7775493a288bc0d20857800c3eeae
hamzath-Ade <hamzathyessoufou@gmail.com>
2023-12-15 11:22:28 +0100

    Modification de dépendences pour l'utilisation binance-api. Ajouts des clés Api dans la classe gestion API.

--- src/main/java/invest/portefeuille/ApiGestion.java
+++ src/main/java/invest/portefeuille/ApiGestion.java
@@ -1,9 +1,14 @@
 package invest.portefeuille;
 
+
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
-import com.binance.api.client.domain.market.TickerPrice;
 
 public class ApiGestion {
 
+    String apiKey = "Sgmx2G8wNNutr00WSPa88Z4wzbNfGrxV4ueYmZOURZFEuIHBdk8M1Ec92LF4itmk"; //Api key
+    String apiSecret = "167zQCKFy94t0IDMOxDEFXp5QpHtQeGDjXX5v5NENUPAZ240yTDS2yKhrXXeyk96"; //Secret key
+
+    BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance(apiKey, apiSecret);
+    BinanceApiRestClient client = factory.newRestClient();
 }

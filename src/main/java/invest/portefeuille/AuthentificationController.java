f41b7ceffd206f7c9889c07c48fcf2b6aa1f4291
Mathusha Parameswaran <mathu251@hotmail.com>
2024-01-12 12:26:36 +0100

    -Sur le Scene builder, déplacer le label au dessus du boutton Valider -le Controller n'est pas encore abouti.

--- src/main/java/invest/portefeuille/AuthentificationController.java
+++ src/main/java/invest/portefeuille/AuthentificationController.java
@@ -20,6 +20,8 @@ public class AuthentificationController {
     @FXML
     private Label labelError;
     @FXML
+    private Label labelError2;
+    @FXML
     private TextField textFieldNom;
     @FXML
     private TextField textFieldPrenom;
@@ -39,6 +41,7 @@ public class AuthentificationController {
         String email = textFieldEmailConnexion.getText();
         String password = textFieldPasswordConnexion.getText();
         Label label =labelError;
+        //Créer une liste qui va reprendre tout les données du fichier CSV
         List<String> existingLines = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(registeredUser))) {
@@ -48,20 +51,23 @@ public class AuthentificationController {
                 existingLines.add(line);
             }
         }
+        //Créer un booléen qui va permettre de vérifier si le mdp et l'email existe dans la base de données
         boolean userExists = existingLines.stream().anyMatch(line -> line.contains(email +";"+ password ));
+        //si vrai, on va pouvoir ouvrir la prochaine page qui est Dashboard
         if (userExists) {
-            FXMLLoader fxmlLoader = new FXMLLoader(AuthentificationController.class.getResource("authentification.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(AuthentificationController.class.getResource("dashboard.fxml"));
             Scene scene = new Scene(fxmlLoader.load(),  320, 240);
 
             Main.authentification.setTitle("Votre compte");
             Main.authentification.setScene(scene);
             Main.authentification.show();
-    }
-        else{
+        }
+        //si faux, on a un message d'erreur
+            else{
             label.setText("Les informations que vous avez fournies sont incorrectes. Veuillez réessayer !");
         }
     }
-/*public void inscription()throws IOException{
+public void inscription()throws IOException{
     String registeredUser = "C:/Users/mathu/IdeaProjects/wallet/Compte.csv";
     String nom= textFieldNom.getText();
     String prenom= textFieldPrenom.getText();
@@ -71,24 +77,29 @@ public class AuthentificationController {
     String passwordConfirm =textFieldPasswordConfirmInscription.getText();
     String cleApi=textFieldCleApi.getText();
     List<String> existingLines = new ArrayList<>();
-    boolean emailExists = existingLines.stream().anyMatch(line -> line.contains(email));
 
+    //Créer un booléen qui va permettre de voir si il est dans le fichier
+    boolean emailExists = existingLines.stream().anyMatch(line -> line.contains(email));
+    //si vrai, on va envoyer un message d'erreur
         if (emailExists) {
-                labelError.setText("Cette adresse e-mail est déjà utilisée");
+                labelError2.setText("Cette adresse e-mail est déjà utilisée");
         }
+        // si faux,on va créer un compte
         else {
+        //si un de données ne sont pas rempli, on va envoyer un message d'erreur
             if (nom.isEmpty() || prenom.isEmpty() || email.isEmpty() || emailConfirm.isEmpty() || password.isEmpty()|| passwordConfirm.isEmpty() || cleApi.isEmpty()) {
-                labelError.setText("Tous les champs doivent être remplis.");
+                labelError2.setText("Tous les champs doivent être remplis.");
             }
+            //Sinon il faut vérifier si les mdp et les emails sont correctes
             else if (!Objects.equals(password, passwordConfirm)||!Objects.equals(email, emailConfirm)){
-                labelError.setText("Les mots de passe ou les mails ne correspondent pas.");
+                labelError2.setText("Les mots de passe ou les mails ne correspondent pas.");
             }
             else {
                 // Ajouter la nouvelle ligne avec le nouvel utilisateur
                 String newUser = nom + ";" + prenom + ";"
                         + email + ";" + password + ";" + cleApi;
                 existingLines.add(newUser);
-                labelError.setText("Votre compte a bien été crée!");
+                labelError2.setText("Votre compte a bien été crée!");
 
                 // Écrire toutes les lignes dans le fichier CSV
                 try (BufferedWriter writer = new BufferedWriter(new FileWriter(registeredUser))) {
@@ -101,5 +112,5 @@ public class AuthentificationController {
 
         }
 
-}*/
+}
 }
